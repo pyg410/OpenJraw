545ca8fe7d822f055de85f0cf6bad3e69a3d88b8
박영규 <pyg410@naver.com>
2026-04-29 22:30:16 +0900

    init

---
+++ src/main/java/io/openjraw/OpenjrawApplication.java
@@ -0,0 +1,13 @@
+package io.openjraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenjrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenjrawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/openjraw/OpenjrawApplicationTests.java
@@ -0,0 +1,13 @@
+package io.openjraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenjrawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
